65a6b841ead9ea0e2a9f59d918ab4fae9c103e99
Lucca Lazzari <lucca.slazzari@gmail.com>
2024-08-14 21:11:22 -0300

    adicionado "match" a função acoesProdutos e pequenas alterações em outras funções

--- main.py
+++ main.py
@@ -16,26 +16,49 @@ class Produto:
         self.qntEstoque = qntEstoque
         self.cat = cat
 
-def addProduto(produto):
-    listaProdutosSis.append(produto)
+def addProduto():
+    print("Insira os dados do novo produto.")
+    nome = input("Nome: ")
+    id = input("ID: ")
+    desc = input("Descrição: ")
+    preco = float(input("Preço: ")) #usar "." para centavos
+    qnt = int(input("Quantidade no estoque: "))
+    cat = input("Categoria: ")
+
+    prod = Produto(nome, id, desc, preco, qnt, cat)
+    listaProdutosSis.append(prod)
 
 def listarProdutos():
     i = 0
+    print("==="*5)
     while i < len(listaProdutosSis):
         print(f"[{i+1}]. {listaProdutosSis[i]}")
         i+=1
+    print("==="*5)
 
 def remProdutoSis():
     esc = input('digite o número do produto que deseja remover')
     listaProdutosSis.pop(esc-1)
 
 def acoesProdutos():
-    print("[1].Listar produtos no estoque\n"+
-        "[2]. Adicionar produto ao estoque\n"+
-        "[3]. Remover produto do estoque\n"+
-        "[0]. Sair")
-    esc = int(input("Escolha o número da operação desejada\n"))
-    while esc<0 or esc>3:
+    while True:
+        print("========"*5+
+            "\n[1]. Listar produtos no estoque\n"+
+            "[2]. Adicionar produto ao estoque\n"+
+            "[3]. Remover produto do estoque\n"+
+            "[0]. Sair\n"+
+            "========"*5)
         esc = int(input("Escolha o número da operação desejada\n"))
+        while esc<0 or esc>3:
+            esc = int(input("Escolha o número da operação desejada\n"))
+        match esc:
+            case 1:
+                listarProdutos()
+            case 2:
+                addProduto()
+            case 3:
+                remProdutoSis()
+        if esc == 0:
+            break
 
 acoesProdutos()
